1213674c40eada315b0e32c5925e51ff68802766
Daniel <dleibox@gmail.com>
2014-09-15 11:31:43 -0400

    init commit

---
+++ JavaConsoleHelloWorld/src/com/firststep/helloworld/Application.java
@@ -0,0 +1,10 @@
+package com.firststep.helloworld;
+
+/**
+ * Created by daniel on 9/15/2014.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
