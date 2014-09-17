a829e5d1615eb9318e37248171a3eebf9bc04b17
daniel <dleibox@gmail.com>
2014-09-17 12:40:33 -0400

    add HelloWorld console tutorial

---
+++ FirstHelloWorld/src/main/java/com/firststep/app/Application.java
@@ -0,0 +1,10 @@
+package com.firststep.app;
+
+/**
+ * Created by daniel on 9/17/2014.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
